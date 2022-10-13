a71661f4d92d9d5f1ee518100aaf767406b2e3ce
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 <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>
2022-10-12 22:02:01 -0300

    better documentation

--- persona.py
+++ persona.py
@@ -8,6 +8,8 @@ class Persona:
         )
 
     def add_Users(self):
+       # Checking if the user is in the database and if it is not, it will add the user to the
+       # database.
         if self.check_input() and not self.is_user_in_DB(self.dni):
             with open(self.database_directory, "a") as f:
                 f.write(self.dni + " " + self.nombre + " " + self.apellido)
@@ -15,6 +17,7 @@ class Persona:
             return True
 
     def check_input(self, dni='', nombre='', apellido=''):
+        # Asking for the user to input a name, surname and dni.
         nombre = input("Ingrese un nombre: ")
         apellido = input("Ingrese apellido: ")
         if dni == '':
@@ -27,6 +30,9 @@ class Persona:
         return True
 
     def refresh_Users(self):
+        """
+        It reads the database file and creates a dictionary of users
+        """
         lines = []
         with open(self.database_directory, "r") as f:
             lines = f.readlines()
@@ -35,6 +41,13 @@ class Persona:
             self.personas[atributos[0]] = atributos
 
     def modify_Users(self, dni):
+        """
+        It opens the file, reads the lines, and then writes the lines back to the file, except for the
+        line that contains the dni
+        
+        :param dni: The DNI of the user you want to modify
+        :return: a boolean value.
+        """
         if self.check_input(dni) and self.is_user_in_DB(dni):
             with open(self.database_directory, "r") as f:
                 lines = f.readlines()
@@ -48,6 +61,12 @@ class Persona:
             return True
 
     def delete_Users(self, dni):
+        """
+        It deletes a user from the database
+        
+        :param dni: The DNI of the user you want to delete
+        :return: A boolean value.
+        """
         if self.is_user_in_DB(dni):
             with open(self.database_directory, "r") as f:
                 lines = f.readlines()
